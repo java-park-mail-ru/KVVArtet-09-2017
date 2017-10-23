5ac7fdb7bcf6dd703217253a411fd6e8100c10eb
Ivan Sopov <moradan228@gmail.com>
2017-10-23 13:25:26 +0300

    DataBase support, Tests

---
+++ src/main/java/states/SignupState.java
@@ -0,0 +1,26 @@
+package states;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import packets.Packet;
+import statemachine.PendingStack;
+
+public class SignupState extends AbstractState {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SignupState.class);
+
+    SignupState(PendingStack stack) {
+        super(stack);
+    }
+
+    @Override
+    public boolean update() {
+        LOGGER.info("SignupState updated");
+        return true;
+    }
+
+    @Override
+    public boolean handlePacket(final Packet packet) {
+        LOGGER.info("SignupState handles packet");
+        return true;
+    }
+}
