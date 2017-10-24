a16aa02e586930e1cd5ace8a60880499eefeb53a
Kirill Cherkasov <kvcherk@live.ru>
2017-10-24 21:16:12 +0300

    -testGetAllUsers() fix

--- src/main/java/states/State.java
+++ src/main/java/states/State.java
@@ -4,7 +4,8 @@ import packets.Packet;
 
 @SuppressWarnings({"UnusedReturnValue", "SameReturnValue", "unused"})
 public interface State {
-    enum StateId { SI_NONE, SI_TITLE, @SuppressWarnings("SpellCheckingInspection") SI_SIGNUP, SI_CHARACTER_LIST, SI_CITY, SI_DUNGEON, SI_CHARACTER_CREATION }
+    enum StateId { SI_NONE, SI_TITLE, @SuppressWarnings("SpellCheckingInspection") SI_SIGNUP, SI_CHARACTER_LIST,
+        SI_CITY, SI_DUNGEON, SI_CHARACTER_CREATION }
 
     void requestStackPush(StateId stateId);
 
