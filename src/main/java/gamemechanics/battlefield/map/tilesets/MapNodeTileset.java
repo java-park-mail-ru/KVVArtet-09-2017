d0108782b7828beb3617aff0a3ffd577d3513f0d
Kirill Cherkasov <kvcherk@live.ru>
2017-11-05 02:17:59 +0300

    game mechanics sandbox:     - Wrote a MapNodeTileset class

--- src/main/java/gamemechanics/battlefield/map/tilesets/MapNodeTileset.java
+++ src/main/java/gamemechanics/battlefield/map/tilesets/MapNodeTileset.java
@@ -21,8 +21,9 @@ public abstract class MapNodeTileset implements Tileset {
         this(center, TilesetShapes.TS_POINT, Directions.DIRECTIONS_COUNT, 0);
     }
 
-    public MapNodeTileset(@NotNull MapNode center, Integer size) {
-        this(center, TilesetShapes.TS_CIRCLE, Directions.DIRECTIONS_COUNT, size);
+    public MapNodeTileset(@NotNull MapNode center, Integer size, Boolean isCircle) {
+        this(center, isCircle ? TilesetShapes.TS_CIRCLE : TilesetShapes.TS_SQUARE,
+                Directions.DIRECTIONS_COUNT, size);
     }
 
     @Override
@@ -38,7 +39,7 @@ public abstract class MapNodeTileset implements Tileset {
     private void makeTileset(@NotNull MapNode center, Integer shape, Integer direction, Integer size) {
         switch (shape) {
             case TilesetShapes.TS_SQUARE:
-                makeSquareTileset(center, direction, size);
+                makeSquareTileset(center, size);
                 break;
             case TilesetShapes.TS_CONE:
                 makeConeTileset(center, direction, size, false);
@@ -57,21 +58,83 @@ public abstract class MapNodeTileset implements Tileset {
         }
     }
 
-    private void makeSquareTileset(@NotNull MapNode center, Integer direction, Integer size) {
+    private void makeSquareTileset(@NotNull MapNode center, Integer size) {
         Integer halfSide = (size - 1) / 2;
-
+        // grabbing row centers, first upper rows, then lower ones
+        List<MapNode> rowCenters = new ArrayList<>();
+        MapNode rowCenter = center.getAdjacent(Directions.UP);
+        while (rowCenter != null) {
+            if (center.getH(rowCenter) > halfSide) {
+                break;
+            }
+            rowCenters.add(0, rowCenter);
+            rowCenter = rowCenter.getAdjacent(Directions.UP);
+        }
+        rowCenters.add(center);
+        rowCenter = center.getAdjacent(Directions.DOWN);
+        while (rowCenter != null) {
+            if (center.getH(rowCenter) > halfSide) {
+                break;
+            }
+            rowCenters.add(rowCenter);
+            rowCenter = rowCenter.getAdjacent(Directions.DOWN);
+        }
+        // for each row center grab a row by two halves and put it into the tileset
+        for (MapNode node : rowCenters) {
+            getTileRowFromRowCenter(node, size, true);
+        }
     }
 
     private void makeConeTileset(@NotNull MapNode center, Integer direction, Integer size, Boolean isReverse) {
-
+        List<MapNode> centralCol = getTilesetLine(center, direction, size);
+        if (centralCol == null) {
+            return;
+        }
+        tileset.addAll(centralCol);
+        if (isReverse) {
+            for (MapNode colCenter : centralCol) {
+                if (center != colCenter) {
+                    Boolean isHorizontal = direction == Directions.LEFT || direction == Directions.RIGHT;
+                    MapNode colHeadLeft = getReverseConeColumnHead(colCenter,
+                            isHorizontal ? Directions.UP : Directions.LEFT);
+                    MapNode colHeadRight = getReverseConeColumnHead(colCenter,
+                            isHorizontal ? Directions.DOWN : Directions.RIGHT);
+                    getReverseConeColumn(colHeadLeft, direction, size - colCenter.getH(colHeadLeft));
+                    getReverseConeColumn(colHeadRight, direction, size - colCenter.getH(colHeadRight));
+                }
+            }
+        } else {
+            Integer colWidth = size;
+            for (MapNode rowCenter : centralCol) {
+                getTileRowFromRowCenter(rowCenter, colWidth--,
+                        direction == Directions.UP || direction == Directions.DOWN);
+            }
+        }
     }
 
     private void makeLineTileset(@NotNull MapNode center, Integer direction, Integer size) {
-
+        tileset.add(center);
+        if (direction < 0 || direction >= Directions.DIRECTIONS_COUNT) {
+            return;
+        }
+        MapNode nextNode = center.getAdjacent(direction);
+        while (nextNode != null) {
+            if (center.getH(nextNode) > size) {
+                break;
+            }
+            if (nextNode.getIsPassable()) {
+                tileset.add(nextNode);
+            } else {
+                break;
+            }
+            nextNode = nextNode.getAdjacent(direction);
+        }
     }
 
     private void makeCircleTileset(@NotNull MapNode center, Integer size) {
-
+        for (Integer direction = Directions.UP; direction < Directions.DIRECTIONS_COUNT; ++direction) {
+            makeConeTileset(center, direction, size, false);
+        }
     }
 
     private void makePointTileset(@NotNull MapNode center) {
@@ -99,11 +162,76 @@ public abstract class MapNodeTileset implements Tileset {
         return line;
     }
 
-    protected Set<MapNode> getTileset() {
+    private MapNode getReverseConeColumnHead(@NotNull MapNode rowCenter, Integer direction) {
+        MapNode colHead = rowCenter.getAdjacent(direction);
+        while (tileset.contains(colHead)) {
+            if (colHead == null) {
+                break;
+            }
+            if (!colHead.getIsPassable()) {
+                break;
+            }
+            colHead = colHead.getAdjacent(direction);
+        }
+        if (colHead != null) {
+            if (!colHead.getIsPassable()) {
+                return null;
+            }
+        }
+        return colHead;
+    }
+
+    private void getReverseConeColumn(@NotNull MapNode colHead, Integer direction, Integer size) {
+        if (colHead != null) {
+            if (size <= 0) {
+                if (!tileset.contains(colHead)) {
+                    tileset.add(colHead);
+                }
+                return;
+            }
+            List<MapNode> column = getTilesetLine(colHead, direction,
+                    size);
+            if (column != null) {
+                for (MapNode node : column) {
+                    if (!tileset.contains(node)) {
+                        tileset.add(node);
+                    }
+                }
+            }
+        }
+    }
+
+    private void getTileRowFromRowCenter(@NotNull MapNode rowCenter, Integer size, Boolean isHorizontal) {
+        Integer halfSize = (size - 1) / 2;
+        if (halfSize <= 0) {
+            return;
+        }
+        Integer sideDirection = isHorizontal ? Directions.LEFT : Directions.UP;
+        List<MapNode> halfRow = getTilesetLine(rowCenter, sideDirection, halfSize);
+        if (halfRow != null) {
+            for (MapNode node : halfRow) {
+                if (!tileset.contains(node)) {
+                    tileset.add(node);
+                }
+            }
+            halfRow.clear();
+        }
+        sideDirection = sideDirection == Directions.LEFT ? Directions.RIGHT : Directions.DOWN;
+        halfRow = getTilesetLine(rowCenter, sideDirection, halfSize);
+        if (halfRow != null) {
+            for (MapNode node : halfRow) {
+                if (!tileset.contains(node)) {
+                    tileset.add(node);
+                }
+            }
+        }
+    }
+
+    Set<MapNode> getTileset() {
         return tileset;
     }
 
-    protected static Integer calculateDirection(@NotNull MapNode sender, @NotNull MapNode target) {
+    static Integer calculateDirection(@NotNull MapNode sender, @NotNull MapNode target) {
         if (sender.equals(target)) {
             return Directions.DIRECTIONS_COUNT;
         }
