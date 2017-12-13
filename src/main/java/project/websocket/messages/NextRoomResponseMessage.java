9448d08564332e2cc82d177a60704085b0736ed5
zonder129 <havofavo@mail.ru>
2017-12-14 02:16:32 +0300

    messages update

--- src/main/java/project/websocket/messages/NextRoomResponseMessage.java
+++ src/main/java/project/websocket/messages/NextRoomResponseMessage.java
@@ -1,4 +1,41 @@
 package project.websocket.messages;
 
+import project.gamemechanics.battlefield.actionresults.ActionResult;
+import project.gamemechanics.battlefield.map.BattleMap;
+import project.gamemechanics.interfaces.AliveEntity;
+
+import java.util.Deque;
+import java.util.List;
+
 public class NextRoomResponseMessage extends Message {
+    private final BattleMap map;
+    private final Deque<AliveEntity> battlersQueue;
+    private final List<ActionResult> battleLog;
+    private Integer turnCounter;
+
+    public NextRoomResponseMessage(BattleMap map,
+                                  Deque<AliveEntity> battlersQueue,
+                                  List<ActionResult> battleLog,
+                                  Integer turnCounter) {
+        this.map = map;
+        this.battlersQueue = battlersQueue;
+        this.battleLog = battleLog;
+        this.turnCounter = turnCounter;
+    }
+
+    public BattleMap getMap() {
+        return map;
+    }
+
+    public Deque<AliveEntity> getBattlersQueue() {
+        return battlersQueue;
+    }
+
+    public List<ActionResult> getBattleLog() {
+        return battleLog;
+    }
+
+    public Integer getTurnCounter() {
+        return turnCounter;
+    }
 }
